6fa6bfad530ef1ad46a2b3bb943955c954874671
aqi00 <aqi00@163.com>
2021-09-22 11:11:47 +0800

    第12章  人脸识别

--- face/src/main/java/com/example/face/util/Utils.java
+++ face/src/main/java/com/example/face/util/Utils.java
@@ -24,38 +24,40 @@ public class Utils {
 
     // 获得屏幕的宽度
     public static int getScreenWidth(Context ctx) {
-        int screenWidth;
-        // 从系统服务中获取窗口管理器
-        WindowManager wm = (WindowManager) ctx.getSystemService(Context.WINDOW_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // 获取当前屏幕的四周边界
-            Rect rect = wm.getCurrentWindowMetrics().getBounds();
-            screenWidth = rect.width();
-        } else {
-            DisplayMetrics dm = new DisplayMetrics();
-            // 从默认显示器中获取显示参数保存到dm对象中
-            wm.getDefaultDisplay().getMetrics(dm);
-            screenWidth = dm.widthPixels;
-        }
-        return screenWidth; // 返回屏幕的宽度数值
+        return ctx.getResources().getDisplayMetrics().widthPixels;
+//        int screenWidth;
+//        // 从系统服务中获取窗口管理器
+//        WindowManager wm = (WindowManager) ctx.getSystemService(Context.WINDOW_SERVICE);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            // 获取当前屏幕的四周边界
+//            Rect rect = wm.getCurrentWindowMetrics().getBounds();
+//            screenWidth = rect.width();
+//        } else {
+//            DisplayMetrics dm = new DisplayMetrics();
+//            // 从默认显示器中获取显示参数保存到dm对象中
+//            wm.getDefaultDisplay().getMetrics(dm);
+//            screenWidth = dm.widthPixels;
+//        }
+//        return screenWidth; // 返回屏幕的宽度数值
     }
 
     // 获得屏幕的高度
     public static int getScreenHeight(Context ctx) {
-        int screenHeight;
-        // 从系统服务中获取窗口管理器
-        WindowManager wm = (WindowManager) ctx.getSystemService(Context.WINDOW_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // 获取当前屏幕的四周边界
-            Rect rect = wm.getCurrentWindowMetrics().getBounds();
-            screenHeight = rect.height();
-        } else {
-            DisplayMetrics dm = new DisplayMetrics();
-            // 从默认显示器中获取显示参数保存到dm对象中
-            wm.getDefaultDisplay().getMetrics(dm);
-            screenHeight = dm.heightPixels;
-        }
-        return screenHeight; // 返回屏幕的高度数值
+        return ctx.getResources().getDisplayMetrics().heightPixels;
+//        int screenHeight;
+//        // 从系统服务中获取窗口管理器
+//        WindowManager wm = (WindowManager) ctx.getSystemService(Context.WINDOW_SERVICE);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            // 获取当前屏幕的四周边界
+//            Rect rect = wm.getCurrentWindowMetrics().getBounds();
+//            screenHeight = rect.height();
+//        } else {
+//            DisplayMetrics dm = new DisplayMetrics();
+//            // 从默认显示器中获取显示参数保存到dm对象中
+//            wm.getDefaultDisplay().getMetrics(dm);
+//            screenHeight = dm.heightPixels;
+//        }
+//        return screenHeight; // 返回屏幕的高度数值
     }
 
     public static Point getSize(Context ctx) {
