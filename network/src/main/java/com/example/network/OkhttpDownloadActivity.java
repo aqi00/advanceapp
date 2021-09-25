f845d42d5001ce0d84b284d7f21297172f501435
aqi00 <aqi00@163.com>
2021-09-25 12:56:38 +0800

    第6章  网络通信

--- network/src/main/java/com/example/network/OkhttpDownloadActivity.java
+++ network/src/main/java/com/example/network/OkhttpDownloadActivity.java
@@ -25,7 +25,7 @@ import okhttp3.Response;
 public class OkhttpDownloadActivity extends AppCompatActivity {
     private final static String TAG = "OkhttpDownloadActivity";
     private final static String URL_IMAGE = "https://img-blog.csdnimg.cn/2018112123554364.png";
-    private final static String URL_APK = "https://ptgl.fujian.gov.cn:8088/masvod/public/2021/03/19/20210319_178498bcae9_r38.mp4";
+    private final static String URL_MP4 = "https://ptgl.fujian.gov.cn:8088/masvod/public/2021/03/19/20210319_178498bcae9_r38.mp4";
     private TextView tv_result; // 声明一个文本视图对象
     private TextView tv_progress; // 声明一个文本视图对象
     private ImageView iv_result; // 声明一个图像视图对象
@@ -80,7 +80,7 @@ public class OkhttpDownloadActivity extends AppCompatActivity {
         iv_result.setVisibility(View.GONE);
         OkHttpClient client = new OkHttpClient(); // 创建一个okhttp客户端对象
         // 创建一个GET方式的请求结构
-        Request request = new Request.Builder().url(URL_APK).build();
+        Request request = new Request.Builder().url(URL_MP4).build();
         Call call = client.newCall(request); // 根据请求结构创建调用对象
         // 加入HTTP请求队列。异步调用，并设置接口应答的回调方法
         call.enqueue(new Callback() {
@@ -97,7 +97,7 @@ public class OkhttpDownloadActivity extends AppCompatActivity {
                 String desc = String.format("文件类型为%s，文件大小为%d", mediaType, length);
                 // 回到主线程操纵界面
                 runOnUiThread(() -> tv_result.setText("下载网络文件返回："+desc));
-                String path = String.format("%s/%s.apk",
+                String path = String.format("%s/%s.mp4",
                         getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString(),
                         DateUtil.getNowDateTime());
                 // 下面从返回的输入流中读取字节数据并保存为本地文件
